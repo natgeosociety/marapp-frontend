793fb283df37cc1fb6fd83311eb9af6cfb91c0d9
ancashoria <horia.ancas@gmail.com>
2020-07-13 17:36:21 +0300

    [EP-2262] Display 404 page on wrong location url

--- packages/earth-map/src/sagas/location/index.ts
+++ packages/earth-map/src/sagas/location/index.ts
@@ -109,7 +109,7 @@ function* toLocation({ payload, meta }) {
     yield put(persistData()); // to keep last viewed place
   } catch (e) {
     // TODO better error handling for sagas
-    if (e.response.status === 403) {
+    if (e.response.status === 403 || e.response.status === 404) {
       replace('/404');
     }
   } finally {
