7ca566febd01069393ea4a4d700d395292e6976d
Bogdan Bruma <bogdan.bruma@qcatalyst.com>
2020-07-13 14:40:52 +0300

    reafctor: $font-weight-bold to $font-weight-medium

--- packages/earth-map/src/utils/index.tsx
+++ packages/earth-map/src/utils/index.tsx
@@ -62,7 +62,7 @@ export const isValidOrg = (orgsFromToken: string[], org: string): boolean =>
 export const parseHintBold = (text: string) => {
   return text.split(/({{.+?}})/).map(term => (
     term.startsWith('{{') && term.endsWith('}}') ?
-    <b className="ng-text-weight-bold-extra">{term.replace('{{', '').replace('}}', '')}</b> :
+    <b className="ng-text-weight-bold">{term.replace('{{', '').replace('}}', '')}</b> :
     term
   ))
 };
