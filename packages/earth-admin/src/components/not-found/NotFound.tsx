783e30316b99f86a7b6aecf4c4539d036921cd26
corinacioloca <corina.cioloca@gmail.com>
2020-08-14 09:55:56 +0300

    [EP-2358] [Marapp/unbl] Invalid organization page is not displayed when invalid org set in URL

--- packages/earth-admin/src/components/not-found/NotFound.tsx
+++ packages/earth-admin/src/components/not-found/NotFound.tsx
@@ -26,7 +26,7 @@ export const NotFound = (props) => {
   const { groups } = useAuth0();
 
   return (
-    <div className="ng-orgswitcher-page">
+    <div className="ng-orgswitcher-page ng-background-ultradkgray">
       <div>
         <h3>404 - Page not found</h3>
         {groups && (
