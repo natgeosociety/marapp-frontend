ca1542df3cda2cc838179d85bdba219c06fecbd9
ancashoria <horia.ancas@gmail.com>
2020-09-07 12:28:01 +0300

    feat(admin): New Organization Landing page [EP-2209]

--- packages/earth-admin/src/pages-client/organizations/home.tsx
+++ packages/earth-admin/src/pages-client/organizations/home.tsx
@@ -3,20 +3,32 @@ import * as React from 'react';
 import { AuthzGuards } from 'auth/permissions';
 import { useAuth0 } from 'auth/auth0';
 import { ContentLayout } from 'layouts';
-import { LinkWithOrg } from 'components';
+import { LinkWithOrg, Card } from 'components';
 
-export function OrganizationHome(props: any) {
+export function OrganizationHome() {
   const { getPermissions } = useAuth0();
   const permissions = getPermissions(AuthzGuards.accessOrganizationsGuard);
   const writePermissions = getPermissions(AuthzGuards.accessOrganizationsGuard);
 
   return (writePermissions && (
     <ContentLayout className="marapp-qa-organizationhome">
-      <div className="ng-flex ng-align-right">
-        <LinkWithOrg className="ng-button ng-button-overlay" to="/organizations/new">
-          add new organization
-        </LinkWithOrg>
-      </div>
+      {writePermissions && (
+        <>
+          <h1 className="ng-text-display-m ng-margin-medium-bottom">ORGANIZATIONS</h1>
+          <div className="ng-grid">
+            <div className="ng-width-1-2">
+              <Card>
+                <p>Start creating a new organization for users to collaborate and share with each other</p>
+                <div className="ng-flex ng-flex-center">
+                  <LinkWithOrg className="marapp-qa-actioncreate ng-button ng-button-secondary" to="organizations/new">
+                    Create new Organization
+                  </LinkWithOrg>
+                </div>
+              </Card>
+            </div>
+          </div>
+        </>
+      )}
     </ContentLayout>
   ));
 }
