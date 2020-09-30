c76a88602029122a6e81d99ccd5b03cfe90c79e2
Bogdan Bruma <bogdan.bruma@qcatalyst.com>
2020-09-30 12:44:08 +0300

    style: explicit boolean cast

--- packages/earth-shared/src/authorization/roles.ts
+++ packages/earth-shared/src/authorization/roles.ts
@@ -31,7 +31,7 @@ enum RoleEnum {
  * @param sep
  */
 export const isSuperAdmin = (roles: string[] & object, sep: string = ':'): boolean => {
-  return roles['*'] || !!roles.find((r: string) => r === ['*', RoleEnum.SUPER_ADMIN].join(sep));
+  return !!roles['*'] || !!roles.find((r: string) => r === ['*', RoleEnum.SUPER_ADMIN].join(sep));
 };
 
 /**
