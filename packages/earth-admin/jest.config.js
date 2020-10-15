589c7b785e03535f2bc107fb13115a79304d0379
ancashoria <horia.ancas@gmail.com>
2020-10-15 14:19:41 +0300

    mocked style imports

--- packages/earth-admin/jest.config.js
+++ packages/earth-admin/jest.config.js
@@ -5,6 +5,12 @@ module.exports = {
   // adds extra assertions like expect().toBeInTheDocument()
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
 
+  // mocks style and image imports
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less|scss)$": "<rootDir>/__mocks__/styleMock.js"
+  },
+
   // moduleNameMapper: {
   //   '^@app/(.*)$': '<rootDir>/src/$1',
   // }
