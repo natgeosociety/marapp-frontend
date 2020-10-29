9a2bc15730850853d134a4f8300cd851645ecf10
Horia Iacos <iacos.horia@gmail.com>
2020-10-29 13:50:15 +0200

    Revert "feat(auth): signup screens (#257)"
    This reverts commit 0b14f802fe72661708ac27567eb7c4d8d6e258aa.

--- packages/earth-auth/src/config.ts
+++
@@ -1,5 +0,0 @@
-export const AuthEnv = {
-  terms: process.env.APP_TERMS_OF_SERVICE,
-  privacy: process.env.APP_PRIVACY_POLICY,
-  atob: process.env.APP_ATOB ? process.env.APP_ATOB : '@@config@@',
-};
