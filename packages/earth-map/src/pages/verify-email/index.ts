06f544c5dc8f996e504dc4f0589d36518c16c12c
Corina Cioloca <corina.cioloca@gmail.com>
2020-11-03 09:36:57 +0200

    feat(map): email validation [EP-2874] (#262)
    * feat(map): [Sign-Up] Validate your email page [EP-2874]
    
    * feat(map): [Sign-Up] Validate your email page [EP-2874]
    
    * chore(auth): cleanup yarn.lock [EP-3111]
    
    * chore(map): changed from pr [EP-2874]
    
    * chore(map): changed from pr [EP-2874]

--- packages/earth-map/src/pages/verify-email/index.ts
+++ packages/earth-map/src/pages/verify-email/index.ts
@@ -17,6 +17,6 @@
   specific language governing permissions and limitations under the License.
 */
 
-import VerifyEmail from './component';
+import VerifyEmail from './VerifyEmail';
 
 export default VerifyEmail;
