98fbbac2781c1b47d09ba14cb92c62ee722acf73
corinacioloca <corina.cioloca@gmail.com>
2020-11-05 11:18:51 +0200

    chore(auth): check if change email page communicates with auth0
    (cherry picked from commit cfaf24fb30dac9bfb17a04fac4ca2932f4a45d97)

--- packages/earth-map/src/pages/change-email/component.tsx
+++ packages/earth-map/src/pages/change-email/component.tsx
@@ -19,46 +19,68 @@
 
 import { useAuth0 } from 'auth/auth0';
 import React, { useEffect } from 'react';
-import { replace } from 'redux-first-router';
 import ProfileService from 'services/ProfileService';
 
 import { Spinner } from '@marapp/earth-shared';
+import { replace } from 'redux-first-router';
+
+enum ChangeEmailStates {
+  VERIFIED = 'Email Change Successful. Please sign in with your original account email to continue with your update.',
+  ERROR = 'Email Update Error',
+  PENDING = 'Change Email. Please sign in with your original account email to continue with your update.',
+}
 
 export default function ChangeEmailComponent() {
-  const { login } = useAuth0();
+  const { login, isAuthenticated } = useAuth0();
 
   useEffect(() => {
-    (async () => {
-      try {
-        const hashParameter = window.location.hash;
-        const hashQuery = hashParameter.split('#')[1];
-        const params = new URLSearchParams(hashQuery);
+    const fn = async () => {
+      if (!isAuthenticated) {
+        return login({
+          appState: { targetUrl: '/' },
+          emailState: ChangeEmailStates['PENDING'],
+        });
+      } else {
+        try {
+          const hashParameter = window.location.hash;
+          const hashQuery = hashParameter.split('#')[1];
+          const params = new URLSearchParams(hashQuery);
 
-        const accessToken = params.get('access_token');
-        const error = params.get('error');
-        const error_description = params.get('error_description');
+          const accessToken = params.get('access_token');
+          const error = params.get('error');
+          const error_description = params.get('error_description');
 
-        if (accessToken) {
-          const response = await ProfileService.changeEmailConfirmation({ accessToken });
-          if (response && response?.data?.success) {
-            alert('Email change successful. Please login using the new credentials.');
-            // Auth0 sessions are reset when a user’s email or password changes;
-            // force a re-login if email change request successful;
-            return login({ appState: { targetUrl: '/' } }); // TODO: redirect to profile after successful change;
-          } else {
-            alert('Could not change email address.');
+          if (accessToken) {
+            console.log('accesstoken');
+            const response = await ProfileService.changeEmailConfirmation({ accessToken });
+            if (response && response?.data?.success) {
+              alert('Email change successful. Please login using the new credentials.');
+              // Auth0 sessions are reset when a user’s email or password changes;
+              // force a re-login if email change request successful;
+              return login({
+                appState: { targetUrl: '/' },
+                emailState: ChangeEmailStates['VERIFIED'],
+              }); // TODO: redirect to profile after successful change;
+            } else {
+              return login({
+                appState: { targetUrl: '/' },
+                emailState: ChangeEmailStates['ERROR'],
+              });
+            }
           }
+          if (error || error_description) {
+            console.error(error, error_description);
+            return login({ appState: { targetUrl: '/' }, emailState: error_description });
+          }
+        } catch (e) {
+          console.error(e);
+        } finally {
+          console.log('finally');
+          replace('/profile');
         }
-        if (error || error_description) {
-          console.error(error, error_description);
-          alert(error_description);
-        }
-      } catch (e) {
-        console.error(e);
-      } finally {
-        replace('/profile');
       }
-    })();
+    };
+    fn();
   });
 
   return <Spinner size="large" />;
