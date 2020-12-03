1886ab98a32ec29fab404bf9d1e71c3e7fbd97ce
vt90 <tomsavlad90@gmail.com>
2020-12-03 23:17:24 +0200

    feat(admin): static translations
    Signed-off-by: vt90 <tomsavlad90@gmail.com>

--- packages/earth-admin/src/components/json-editor/JsonEditor.tsx
+++ packages/earth-admin/src/components/json-editor/JsonEditor.tsx
@@ -25,6 +25,7 @@ import { UnControlled as CodeMirror } from 'react-codemirror2';
 import { useTranslation } from 'react-i18next';
 
 import './styles.scss';
+import { useTranslation } from 'react-i18next';
 
 interface JsonEditorProps {
   json?: {};
