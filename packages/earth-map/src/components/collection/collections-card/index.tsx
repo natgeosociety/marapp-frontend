dfdae747e37f7c22e0cfe7b5005240273d6aeb03
Vlad Tomsa <tomsavlad90@gmail.com>
2021-01-21 14:40:37 +0200

    wip - update from dev
    Signed-off-by: Vlad Tomsa <tomsavlad90@gmail.com>

---
+++ packages/earth-map/src/components/collection/collections-card/index.tsx
@@ -0,0 +1,38 @@
+/*
+  Copyright 2018-2020 National Geographic Society
+
+  Use of this software does not constitute endorsement by National Geographic
+  Society (NGS). The NGS name and NGS logo may not be used for any purpose without
+  written permission from NGS.
+
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+  this file except in compliance with the License. You may obtain a copy of the
+  License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software distributed
+  under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+  CONDITIONS OF ANY KIND, either express or implied. See the License for the
+  specific language governing permissions and limitations under the License.
+*/
+
+import React from 'react';
+
+import useLocations from '../../../fetchers/useLocations';
+import { LocationTypeEnum } from '../../../modules/places/model';
+import { CollectionsCard } from './CollectionsCard';
+
+export default function WithData(props) {
+  const { group } = props;
+
+  const { data } = useLocations({
+    select: 'slug,name,id,organization,type,updatedAt',
+    filter: ['type', '==', LocationTypeEnum.COLLECTION].join(''),
+    page: { size: 5 },
+    sort: '-updatedAt',
+    group: group.toString(),
+  });
+
+  return <CollectionsCard data={data} {...props} />;
+}
